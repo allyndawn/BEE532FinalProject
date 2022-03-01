af8036c7ce6f0d69c4e512f992e7385ef2e9fbb9
allendavidsnook <allendavidsnook@gmail.com>
2022-02-28 17:36:42 -0800

    Tweak params to match 2021 Perrot example, start adding indexing

--- __main__.py
+++ __main__.py
@@ -4,16 +4,16 @@ import numpy
 # Constants
 speed_of_sound = 1540 # m/s
 
-# Transducer Config, e.g. ATL L7-4
-center_frequency = 5.2E6 # frequency in MHz
+# Transducer Config
+center_frequency = 7600000 # frequency in MHz
 sampling_rate = 4 * center_frequency # sampling rate in MHz
 num_elements = 128
-pitch = 0.000245 # pitch in m, e.g. 0.000245 = 0.245 mm
+pitch = 3E-4 # pitch in m
 
 # Scan Config
-theta = numpy.deg2rad(0)
-beta = numpy.deg2rad(30)
-scan_depth = 0.10 # scan depth in m, e.g. 0.10 = 10 cm
+theta = numpy.deg2rad(0) # desired tilt from the z axis in radians
+beta = numpy.deg2rad(0.1) # desired beamwidth in radians (plane waves approach 0)
+scan_depth = 0.04589 # scan depth in m, e.g. 0.10 = 10 cm
 
 class Point:
     x: float = 0.0
@@ -73,14 +73,18 @@ if __name__ == "__main__":
     virtual_source_y = 0.0
     virtual_source_z = (-array_width / 2) * (numpy.cos(beta) + numpy.cos(2 * theta)) / numpy.sin(beta)
 
+    print("virtual_source_x", virtual_source_x)
+    print("virtual_source_z", virtual_source_z)
+
     virtual_source_position = Point(virtual_source_x, virtual_source_y, virtual_source_z)
 
-    # Define a (single) focus centered 5 cm below the face of the transducer
+    # Define a (single) focus centered 3 cm below the face of the transducer
     # For now, don't allow the focus y to be non zero
-    focus_position = Point(0, 0, 0.05)
+    focus_position = Point(0, 0, 0.03)
 
     # Quick and dirty f-number needed for that depth
     f_number = focus_position.z / array_width
+    print("f_number", f_number)
 
     # Calculate distances_rx from the focus_position to each transducer element
     rx_distances = []
@@ -107,12 +111,20 @@ if __name__ == "__main__":
     # Convert each two way travel time into the corresponding fast-time index
     # by multiplying the sampling frequency (e.g. 20E6 samples/sec) by the
     # time offset
+    #
+    # Remember, in ultrasound "fast time" refers to the time domain in which we wait for one
+    # transmit pulse to propagate and reflect. "Slow time" refers to the domain in which we
+    # wait for "fast time" to complete for a given pulse before moving to the next pulse.
+    #
+    # e.g. values for the fast_time_indices may range from 1294.7 for the edge elements
+    # to as little as 1185.4 for the center elements
     fast_time_indices = []
     for el in range(num_elements):
         fast_time_index = two_way_travel_times[el] * sampling_rate
         fast_time_indices.append(fast_time_index)
 
     # Generate a true/false vector based on whether each element's fast time index is within bounds
+    # e.g. Num samples per line = 1812
     fast_time_index_in_bounds_array = []
     for el in range(num_elements):
         fast_time_index_in_bounds = fast_time_indices[el] < num_samples_per_line
@@ -126,8 +138,37 @@ if __name__ == "__main__":
         focus_in_element_field_of_view = delta_x < scaled_z
         focus_in_element_field_of_view_array.append(focus_in_element_field_of_view)
 
-    # Combine these into a single vector on whether those two constains are both met
+    # Combine these into a single vector on whether those two constraints are both met
     element_is_viable_for_beamforming_to_focus_array = []
     for el in range(num_elements):
         element_is_viable_for_beamforming_to_focus = fast_time_index_in_bounds_array[el] and \
             focus_in_element_field_of_view_array[el]
+        element_is_viable_for_beamforming_to_focus_array.append(focus_in_element_field_of_view_array)
+
+    # We want to at this point create a massive column vector that we can multiply the input signal
+    # by to shift each element's samples to properly beamform them.
+
+    # The first step is to create an indexing row vector for each element
+    # To do this, we take the fast_time_indices and add to each "index" an offset
+    # into the massive column vector (except the first element, which has an offset of 0)
+    #
+    # For example, the fast_time_index for the first element has 0 added to it,
+    # the fast_time_index for the second element has num_samples_per_line added to it,
+    # the third, 2 * num_samples_per_line and so on and so forth
+    #
+    # This spaces out all the lines so they don't overlap when we pack them in the massive column vector
+
+    index_array = []
+    for el in range(num_elements):
+        index = fast_time_indices[el] + el * num_samples_per_line
+        index_array.append(index)
+
+    # We then discard all those elements that are not viable for beamforming
+    viable_index_array = []
+    for el in range(num_elements):
+        if element_is_viable_for_beamforming_to_focus_array[el]:
+            viable_index_array.append(index_array[el])
+
+    num_viable_elements = len(viable_index_array)
+    print(num_viable_elements)
+
